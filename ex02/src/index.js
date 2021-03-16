9258d72a0c1c9925d9c61a9b764e851ca8dfc626
nejira-k <kadric.nejira@gmail.com>
2021-03-16 09:03:21 +0100

    Added new files

---
+++ ex02/src/index.js
@@ -0,0 +1,8 @@
+// Write code below this line
+function functionPrintInfor( name, age ) {
+    console.log( name, age );
+}
+functionPrintInfor( "John", 23);
+functionPrintInfor( "Michael", 43);
+// Write code above this line
+module.exports = functionPrintInfor;
---
+++ ex07/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+}
+addThree();
+console.log(sum);
+function addFive() {
+    sum = sum + 5;
+}
+addFive();
+console.log(sum);
+// Only change code above this line
+module.exports = {
+    addThree,
+    addFive
+};
